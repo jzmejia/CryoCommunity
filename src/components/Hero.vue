13dae6ec05f0febb848c7e8d909d7566afbe45e9
Jessica Mejia <jzimmerman990@gmail.com>
2021-08-30 21:25:36 -0400

    updated hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,16 +1,14 @@
 <template>
-  <v-parallax 
-  :src="heroData.hero.heroImgSrc">
-
-  <!-- <v-parallax 
-    dark
-    src="~/assets/celia-terminus.jpg"> -->
+  <v-parallax
+  :src="heroData.hero.heroImgSrc"
+  height="900"
+  >
     <v-row align="center" justify="center">
       <v-col/>
       <v-col>
-        <h1 class="h1 font-weight-bold text-center">{{heroData.hero.heroTitle}}</h1>
+        <h1 class="h1 font-weight-bold text-center black--text">{{heroData.hero.heroTitle}}</h1>
         <br />
-        <h3 class="h3 text-center">{{heroData.hero.heroSubtitle}}</h3>
+        <h3 class="h3 text-center black--text">{{heroData.hero.heroSubtitle}}</h3>
       </v-col>
       <v-col/>
     </v-row>
