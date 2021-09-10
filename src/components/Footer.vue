64dae71018f25cc85fcef9b60f53e377b47214ba
Jessica Mejia <jzimmerman990@gmail.com>
2021-09-10 14:37:31 -0400

    Fixed error

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,8 +7,9 @@
           <v-list subheader>
             <v-subheader>site map</v-subheader>
             <v-list-item-content>
-              <v-list-item-title>
+              <v-list-item-title></v-list-item-title>
             </v-list-item-content>
+          </v-list>
         </v-col>
         <v-col cols=3></v-col>
         <v-col cols=3></v-col>
