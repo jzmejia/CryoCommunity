610ba24983ff2a5959a63dd83623957dbbf7958c
Jorge <liberaltech@users.noreply.github.com>
2021-09-18 13:20:57 -0400

    added vuetify-loader

--- gridsome.server.js
+++ gridsome.server.js
@@ -4,34 +4,17 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
-const nodeExternals = require('webpack-node-externals')
-// const axios = require("axios");
-module.exports = function (api) {
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
+module.exports = (api) => {
   api.chainWebpack((config, { isServer }) => {
-    
-    if (isServer) {
-      config.externals([
-        nodeExternals({
-          allowlist: [/^vuetify/]
-        })
-      ])
-    }
-    // config.devServer
-    //       .public("localhost:8080")
-    //       .host("localhost")
-    //       .port(8080)
-    //       .hotOnly(true)
-    //       .watchOptions({ poll: 1000 })
-    //       .https(false)
-    //       .disableHostCheck(true)
-    //       .headers({ "Access-Control-Allow-Origin": ["*"] });
-  })
+    config.plugin('vuetify-loader').use(VuetifyLoaderPlugin);
+  });
   
   // api.loadSource(async({ addCollection }) => {
 
   // })
 
-  api.createPages(({ createPage }) => {
+  // api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api/
-  })
+  // })
 }
