3e6b776dcedb002742afd8275d8e20e2f1bb0e54
Jessica Mejia <jzmejia990@gmail.com>
2024-11-03 16:50:12 -0500

    adding gear resources

--- src/components/Gear/RequestForm.vue
+++ src/components/Gear/RequestForm.vue
@@ -1,36 +1,60 @@
 <template>
   <v-card min-height="400">
-    <v-card-title class="py-2">
-      Request
-    </v-card-title>
+    <v-card-title class="py-2"> Request </v-card-title>
     <v-divider />
     <v-card-text>
-      <v-form>
+      <!-- <v-form>
         <v-text-field dense outlined label="field-1"></v-text-field>
         <v-text-field dense outlined label="field-2"></v-text-field>
         <v-btn type="submit" depressed block>send request</v-btn>
-      </v-form>
+      </v-form> -->
+
+      <v-list-item
+        href="https://docs.google.com/forms/d/e/1FAIpQLSeyQLTjbscKJC3HXeLy16AkkJpnvYLytVCrJp5YnRefiX-uAA/viewform"
+      >
+        <v-list-item-title>Make a Request</v-list-item-title>
+      </v-list-item>
+
+      <v-list-item
+        href="https://docs.google.com/spreadsheets/d/17yhJivJzU526hdAFKWWxq6S7nrTDBYPx25libBDPioI/edit?gid=898670185#gid=898670185"
+      >
+        <v-list-item-title>Browse Current Requests</v-list-item-title></v-list-item
+      >
+
+      <v-list-item
+        href="https://docs.google.com/spreadsheets/d/17yhJivJzU526hdAFKWWxq6S7nrTDBYPx25libBDPioI/edit?gid=84482305#gid=84482305"
+      >
+        <v-list-item-title> Browse Available Gear </v-list-item-title>
+      </v-list-item>
+      <v-list-item href="https://docs.google.com/spreadsheets/d/17yhJivJzU526hdAFKWWxq6S7nrTDBYPx25libBDPioI/edit?gid=22125841#gid=22125841">
+        <v-list-item-title>Community Expectations</v-list-item-title>
+        </v-list-item>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
-const formURL = "https://docs.google.com/forms/d/e/1FAIpQLSeJWs2XsUN_M8wSQvkg87HT3CvI7HIjsW8_tbBBW9NaFvA_xQ/viewform?usp=sf_link";
-const sheetURL = "https://docs.google.com/spreadsheets/d/17yhJivJzU526hdAFKWWxq6S7nrTDBYPx25libBDPioI/pub?output=csv"
-const CLIENT_ID = "616013307584-f2pjc6409fg7nnstuj5v0p73uq0d1lbi.apps.googleusercontent.com"
-const API_KEY = "AIzaSyDdhHk_UzlzXcB1ks1KIFh4e3nvOzgtY7Y"
+const formURL =
+  "https://docs.google.com/forms/d/e/1FAIpQLSeJWs2XsUN_M8wSQvkg87HT3CvI7HIjsW8_tbBBW9NaFvA_xQ/viewform?usp=sf_link";
+const sheetURL =
+  "https://docs.google.com/spreadsheets/d/17yhJivJzU526hdAFKWWxq6S7nrTDBYPx25libBDPioI/pub?output=csv";
+const CLIENT_ID =
+  "616013307584-f2pjc6409fg7nnstuj5v0p73uq0d1lbi.apps.googleusercontent.com";
+const API_KEY = "AIzaSyDdhHk_UzlzXcB1ks1KIFh4e3nvOzgtY7Y";
 export default {
   data: () => ({
     isActive: false,
   }),
-	mounted(){
-		this.get()
-	},
+  mounted() {
+    this.get();
+  },
   methods: {
-		get(){
-			const data = fetch("https://sheets.googleapis.com/v4/spreadsheets/17yhJivJzU526hdAFKWWxq6S7nrTDBYPx25libBDPioI")
-			console.log(data)
-		},
+    get() {
+      const data = fetch(
+        "https://sheets.googleapis.com/v4/spreadsheets/17yhJivJzU526hdAFKWWxq6S7nrTDBYPx25libBDPioI"
+      );
+      console.log(data);
+    },
     toggle() {
       this.isActive = !this.isActive;
     },
--- src/components/Gear/Resources.vue
+++ src/components/Gear/Resources.vue
@@ -1,9 +1,34 @@
 <template>
     <v-list subheader class="py-0">
-        <v-list-item :ripple="false" link v-for="i in 15" :key="i">
-            <v-list-item-content>
-                <v-list-item-title class="font-italic">resource {{ i }}</v-list-item-title>
-            </v-list-item-content>
+       
+        <v-list-item href="https://docs.google.com/spreadsheets/d/17yhJivJzU526hdAFKWWxq6S7nrTDBYPx25libBDPioI/edit?gid=1889059145#gid=1889059145">
+            <v-list-item-title>Example Packing Lists</v-list-item-title>
+        </v-list-item>
+        <v-list-item href="https://docs.google.com/forms/d/e/1FAIpQLSfwArFnCkS-LULws1zJjV8_loCiKpzN0C0iedtDm_EFMJ2zUA/viewform">
+            <v-list-item-title>Add a packing list to database</v-list-item-title>
+        </v-list-item>
+        <v-list-item href="https://docs.google.com/spreadsheets/d/17yhJivJzU526hdAFKWWxq6S7nrTDBYPx25libBDPioI/edit?gid=1507354125#gid=1507354125">
+            <v-list-item-title>Gear Stores</v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+            <v-list-title>Pro Deals and Discounts</v-list-title>
+        </v-list-item>
+        <v-list-item>
+            <v-list-item-title>Gear Repair Resources</v-list-item-title>
+        </v-list-item>
+        <v-list-item href="https://cryocommunity.org/resources/#fieldwork">
+            <v-list-item-title>
+                Fieldwork Guides
+            </v-list-item-title>
+        </v-list-item>
+        <v-list-item href="https://alwaystheadventure.com/sustainability/used-gear-stores#NorthCarolina=">
+            <v-list-item-title>Used Outdoor Gear Store Guide in the U.S.</v-list-item-title>
+        </v-list-item>
+        <v-list-item href="https://www.melaninbasecamp.com/trip-reports/2018/1/28/ballin-on-a-budget?rq=Broke%20girl">
+            <v-list-item-title>A Broke Girl's Guide to Getting Gear</v-list-item-title>
+        </v-list-item>
+        <v-list-item href="https://www.melaninbasecamp.com/trip-reports/2021/4/1/a-broke-girls-guide-to-getting-gear-part-2">
+            <v-list-item-title>A Broke Girl's Guide to Getting Gear Part 2</v-list-item-title>
         </v-list-item>
     </v-list>
 </template>
