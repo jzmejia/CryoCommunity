fa7214ac2d0740895eca7cb823422b3569dcb10c
marionmckenzie <marion.mckenzie@mines.edu>
2025-01-13 21:00:09 +0000

    formatting fix

--- src/components/Gear/Resources.vue
+++ src/components/Gear/Resources.vue
@@ -1,21 +1,21 @@
 <template>
     <v-list subheader class="py-0">
-       
-        <v-list-item href="https://docs.google.com/spreadsheets/d/17yhJivJzU526hdAFKWWxq6S7nrTDBYPx25libBDPioI/edit?gid=1889059145#gid=1889059145">
-            <v-list-item-title>Example Packing Lists</v-list-item-title>
-        </v-list-item>
+        <v-list-item-content>
+            <v-list-item href="https://docs.google.com/spreadsheets/d/17yhJivJzU526hdAFKWWxq6S7nrTDBYPx25libBDPioI/edit?gid=1889059145#gid=1889059145">
+                <v-list-item-title>Example Packing Lists</v-list-item-title>
+            </v-list-item>
         <v-list-item href="https://docs.google.com/forms/d/e/1FAIpQLSfwArFnCkS-LULws1zJjV8_loCiKpzN0C0iedtDm_EFMJ2zUA/viewform">
             <v-list-item-title>Add a packing list to database</v-list-item-title>
         </v-list-item>
         <v-list-item href="https://docs.google.com/spreadsheets/d/17yhJivJzU526hdAFKWWxq6S7nrTDBYPx25libBDPioI/edit?gid=1507354125#gid=1507354125">
             <v-list-item-title>Gear Stores</v-list-item-title>
         </v-list-item>
-        <!-- <v-list-item>
+        < <v-list-item href="https://docs.google.com/spreadsheets/d/17yhJivJzU526hdAFKWWxq6S7nrTDBYPx25libBDPioI/edit?usp=sharing">
             <v-list-title>Pro Deals and Discounts</v-list-title>
-        </v-list-item> -->
-        <v-list-item>
-            <v-list-item-title>Gear Repair Resources</v-list-item-title>
         </v-list-item>
+        <!-- v-list-item>
+            <v-list-item-title>Gear Repair Resources</v-list-item-title>
+        </v-list-item> -->
         <v-list-item href="https://cryocommunity.org/resources/#fieldwork">
             <v-list-item-title>
                 Fieldwork Guides
@@ -35,6 +35,9 @@
         </v-list-item>
         <v-list-item href="https://www.earthscope.org/project-support-request/">
         <v-list-item-title>EarthScope Consortium Resources</v-list-item-title>
+        </v-list-item>
+        <v-list-item href="https://juneauicefield.org/expedition/gear-list">
+        <v-list-item-title>Juneau Ice Field Research Program used gear resources and participant forum</v-list-item-title>
         </v-list-item>
     </v-list>
 </template>
